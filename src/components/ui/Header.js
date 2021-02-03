42455fc2f6a4b280d874af1a67418f78c6320f61
rohan <rohanrajgautam@gmail.com>
2021-02-03 22:59:41 +0545

    Package installation and Appbar component

---
+++ src/components/ui/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+export default function Header(props) {
+  return (
+    <AppBar>
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  );
+}
