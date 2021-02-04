eb44ddfc5ab9928730202a00f264c2ca648d118a
rohan <rohanrajgautam@gmail.com>
2021-02-04 12:16:42 +0545

    header component with tabs and styling, and react-router-dom intregation

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import Header from './ui/Header';
 import { ThemeProvider } from '@material-ui/styles';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import theme from './ui/Theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      hello from rrg
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={() => <div>Home</div>} />
+          <Route exact path='/services' component={() => <div>Services</div>} />
+          <Route
+            exact
+            path='/customsoftware'
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path='/mobileapps'
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route
+            exact
+            path='/revolution'
+            component={() => <div>Revolution</div>}
+          />
+          <Route exact path='/about' component={() => <div>About</div>} />
+          <Route exact path='/contact' component={() => <div>Contact</div>} />
+          <Route exact path='/estimate' component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   AppBar,
   Toolbar,
@@ -6,15 +6,58 @@ import {
   Zoom,
   makeStyles,
   Fab,
-  Typography,
+  Tabs,
+  Tab,
+  Button,
 } from '@material-ui/core';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+
 const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em',
+    [theme.breakpoints.down('md')]: {
+      marginBottom: '2em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '1.25em',
+    },
+  },
+  logo: {
+    height: '8em',
+    [theme.breakpoints.down('md')]: {
+      height: '7em',
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '5.5em',
+    },
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px',
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+    },
   },
 }));
 
@@ -46,15 +89,82 @@ function ScrollTop(props) {
 }
 
 export default function Header(props) {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    }
+  }, [value]);
+
   return (
     <>
       <AppBar position='fixed'>
-        <Toolbar>
-          <Typography variant='h3' color='secondary'>
-            MUI Development
-          </Typography>
+        <Toolbar disableGutters>
+          <Button
+            component={Link}
+            to='/'
+            disableRipple
+            onClick={() => setValue(0)}
+            className={classes.logoContainer}
+          >
+            <img src={logo} className={classes.logo} alt='company logo' />
+          </Button>
+          <Tabs
+            onChange={handleChange}
+            value={value}
+            className={classes.tabContainer}
+            indicatorColor='primary'
+          >
+            <Tab className={classes.tab} component={Link} to='/' label='Home' />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              label='Services'
+              to='/services'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/revolution'
+              label='The Revolution'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/about'
+              label='About Us'
+            />
+            <Tab
+              className={classes.tab}
+              component={Link}
+              to='/contact'
+              label='Contact Us'
+            />
+          </Tabs>
+          <Button
+            variant='contained'
+            color='secondary'
+            className={classes.button}
+          >
+            Free Estimate
+          </Button>
         </Toolbar>
       </AppBar>
+      <div className={classes.toolbarMargin} />
       <Toolbar id='back-to-top-anchor' />
       <ScrollTop {...props}>
         <Fab color='secondary' size='small' aria-label='scroll back to top'>
