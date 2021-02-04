a47df7aa639200fbc7887a3cee82dae2b203f9fe
rohan <rohanrajgautam@gmail.com>
2021-02-04 09:46:07 +0545

    theme setup

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,66 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  useScrollTrigger,
+  Zoom,
+  makeStyles,
+  Fab,
+  Typography,
+} from '@material-ui/core';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function ScrollTop(props) {
+  const { children } = props;
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor'
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role='presentation' className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
 
 export default function Header(props) {
   return (
-    <AppBar>
-      <Toolbar>Arc Development</Toolbar>
-    </AppBar>
+    <>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography variant='h3' color='secondary'>
+            MUI Development
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id='back-to-top-anchor' />
+      <ScrollTop {...props}>
+        <Fab color='secondary' size='small' aria-label='scroll back to top'>
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+    </>
   );
 }
